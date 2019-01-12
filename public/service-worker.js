089041311abb0b3d54cfee62f342bdd47c1756f2
Muhammad Edward Chakim <muhedwardc@gmail.com>
2019-01-12 10:43:43 +0700

    sw

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,5 +1,41 @@
 if (workbox) {
+    workbox.setConfig({
+        debug: false,
+    });
+
     self.__precacheManifest = [].concat(self.__precacheManifest || []);
-    workbox.precaching.suppressWarnings();
     workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+    
+    workbox.precaching.suppressWarnings();
+    workbox.routing.registerRoute(
+    /\.(?:png|gif|jpg|jpeg|svg)$/,
+    workbox.strategies.staleWhileRevalidate({
+        cacheName: 'images',
+        plugins: [
+        new workbox.expiration.Plugin({
+            maxEntries: 60,
+            maxAgeSeconds: 30 * 24 * 60 * 60, // 30 Days
+        }),
+        ],
+    }),
+    );
+    
+    workbox.routing.registerRoute(
+    new RegExp('http://api.post.test'),
+    workbox.strategies.networkFirst({
+        cacheName: 'api',
+    }),
+    );
+    
+    workbox.routing.registerRoute(
+    new RegExp('https://fonts.(?:googleapis|gstatic).com/(.*)'),
+    workbox.strategies.cacheFirst({
+        cacheName: 'googleapis',
+        plugins: [
+        new workbox.expiration.Plugin({
+            maxEntries: 30,
+        }),
+        ],
+    }),
+    );
 }
